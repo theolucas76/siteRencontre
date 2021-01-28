735e41d87caa163d28f6f69eea3eda6a9803ef09
theolucas76 <76164072+theolucas76@users.noreply.github.com>
2021-01-28 16:46:46 +0100

    fin de journée

--- views/lovers.php
+++ views/lovers.php
@@ -17,11 +17,11 @@
                 <nav class="navbar navbar-expand-lg navbar-dark">
                     <div class="container-fluid">
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01"
+                            data-bs-target="#navbarNav" aria-controls="navbarNav"
                             aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
-                        <div class="collapse navbar-collapse">
+                        <div class="collapse navbar-collapse" id="navbarNav">
                             <ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-flex">
                                 <li class="nav-item">
                                     <a class="nav-link text-white">Bienvenue <?= $_COOKIE['firstname']?></a>
